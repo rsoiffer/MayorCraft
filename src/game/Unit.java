6eac3b351d28854bf81742714fb2cbc339f81961
Chris Goodman <chrisgoodman14@gmail.com>
2015-02-27 09:14:36 -0800

    Add destination component and system

---
+++ src/game/Unit.java
@@ -0,0 +1,21 @@
+package game; 
+
+import core.AbstarctEntity; 
+import core.Keys;
+import core.MouseInput;
+import movement.*;
+
+public class Unit extends AbstractEntity {
+	public Unit(){
+		PositionComponent pc = add(new PositionComponent());
+		RotationComponent rc = add(new VelocityComponent());
+		
+	}
+	public void followOrders(){
+
+
+	}
+
+
+
+}
---
+++ src/movement/DestinationSystem.java
@@ -0,0 +1,15 @@
+package movement; 
+
+import core.AbstractSystem;
+
+public class DestinationSystem extends AbstractSystem{
+
+private PositionComponent position;
+private VelocityComponent velocity;
+private DestinationComponent destination;
+
+public DestinationSystem(PositionComponent position, VelocityComponent velocity, DestinationComponent destination){
+
+
+}
+}
