2f72c3e4089b5a686cf48520a3d9aa56820f4292
Weirdbob95 <rory.soiffer@comcast.net>
2015-02-27 12:30:36 -0800

    Update World.java

--- src/worldgen/World.java
+++ src/worldgen/World.java
@@ -7,12 +7,17 @@ public class World extends AbstractEntity {
     public World() {
         //Create world code
         /*
-Step 1: Choose a ton of random points
-Step 2: Calculate their Voronoi polygons through Fortune's algorithm
-Step 3: Smooth the distribution by replacing each point with the average of its polygon's point
-Step 4: Recalculate the Voronoi polygons
-Step 5: Determine whether each polygon is land or water
-Step 6: Calculate the height of each polygon
+Choose a ton of random points
+Calculate their Voronoi polygons through Fortune's algorithm
+Smooth the distribution by replacing each point with the average of its polygon's point
+Recalculate the Voronoi polygons
+Determine whether each corner is land or water
+Set the height at the borders to 0, add them to the elevation queue, set the rest to infinite height
+While the queue is nonempty, grab the first corner
+    For each of its neighbors
+        If the path to the neighbor is shorter, set the neighbor's elevation and downslope, add it to queue
+Sort the corners by elevation
+Set each elevation to 1 - sqrt(1 - index/length)
         */
     }
 }
