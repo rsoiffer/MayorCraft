0bbf891a9aa5817d478c4ac9463ff21e84170c62
Weirdbob95 <rory.soiffer@comcast.net>
2015-02-27 11:17:20 -0800

    Update World.java

--- src/game/World.java
+++ src/game/World.java
@@ -6,5 +6,12 @@ public class World extends AbstractEntity {
 
     public World() {
         //Create world code
+        /*
+Step 1: Choose a ton of random points
+Step 2: Calculate their Voronoi polygons through Fortune's algorithm
+Step 3: Smooth the distribution by replacing each point with the average of its polygon's point
+Step 4: Recalculate the Voronoi polygons
+
+        */
     }
 }
