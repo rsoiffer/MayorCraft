74de94c2a1e46f7859e110d7373d326369423d93
RSoiffer16 <RSoiffer16@S-RM43-07.jesuitpdx.org>
2015-03-18 17:29:38 -0700

    working on animations

--- src/units/AnimationSystem.java
+++ src/units/AnimationSystem.java
@@ -1,7 +1,12 @@
 package units;
 
 import core.AbstractSystem;
+import core.Color4d;
 import core.Vec2;
+import graphics.Graphics;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
 import movement.PositionComponent;
 import movement.RotationComponent;
 import movement.VelocityComponent;
@@ -22,16 +27,10 @@ public class AnimationSystem extends AbstractSystem {
 
     @Override
     public void update() {
-        System.out.println(pc.pos);
-        System.out.println(vc.vel);
         double speed = vc.vel.length();
         Vec2 direction = vc.vel.multiply(1 / speed);
-//        ac.legL.target = pc.pos.add(direction.multiply(30)).add(direction.normal().multiply(8));
-//        ac.legR.target = pc.pos.add(direction.multiply(30)).add(direction.normal().multiply(-8));
-//        double distL2 = ac.legL.target.subtract(ac.legL.pos).lengthSquared();
-//        if ()
-        //optimization code
 
+        //optimization code
         double s_d = direction.y;
         double c_d = direction.x;
 
@@ -87,5 +86,116 @@ public class AnimationSystem extends AbstractSystem {
             ac.legL.pos = new Vec2(pc.pos.x - 8 * s_d, pc.pos.y - 8 * c_d);
             ac.legR.pos = new Vec2(pc.pos.x + 8 * s_d, pc.pos.y + 8 * c_d);
         }
+
+        //RENDER CODE
+        ac.legL.draw(rc.rot);
+        ac.legR.draw(rc.rot);
+        //draw_sprite_ext(foot_sprite,0,lfoot_x,lfoot_y,1,1,facing_direction,c_white,1);
+        //draw_sprite_ext(foot_sprite,0,rfoot_x,rfoot_y,1,1,facing_direction,c_white,1);
+
+//        lleg_x = x - 6 * sin(facing_direction * pi / 180);
+//        lleg_y = y - 6 * cos(facing_direction * pi / 180);
+//        rleg_x = x + 6 * sin(facing_direction * pi / 180);
+//        rleg_y = y + 6 * cos(facing_direction * pi / 180);
+        /*
+         lknee_x=(lleg_x+lfoot_x)/2+6*cos(facing_direction*pi/180);
+         lknee_y=(lleg_y+lfoot_y)/2-6*sin(facing_direction*pi/180);
+         rknee_x=(rleg_x+rfoot_x)/2+6*cos(facing_direction*pi/180);
+         rknee_y=(rleg_y+rfoot_y)/2-6*sin(facing_direction*pi/180);
+
+         draw_set_color(rleg_color);
+         draw_line_width(rfoot_x-2*cos(facing_direction*pi/180), rfoot_y+2*sin(facing_direction*pi/180), rknee_x, rknee_y, 8);
+         draw_circle(rknee_x, rknee_y, 4, false);
+         draw_line_width(rleg_x, rleg_y, rknee_x, rknee_y, 8);
+         //draw_line_width(rfoot_x, rfoot_y, rleg_x, rleg_y, 4);
+         if(has_pegleg){
+         draw_set_color(c_white);
+         draw_sprite_ext(pegleg,0,lleg_x,lleg_y,sqrt(sqr(lleg_x-lfoot_x)+sqr(lleg_y-lfoot_y))/32,1,arctan2(lleg_y-lfoot_y,lfoot_x-lleg_x)*180/pi,c_white,1);
+         }
+         else{
+         draw_set_color(lleg_color);
+         draw_line_width(lfoot_x-2*cos(facing_direction*pi/180), lfoot_y+2*sin(facing_direction*pi/180), lknee_x, lknee_y, 8);
+         draw_circle(lknee_x, lknee_y, 4, false);
+         draw_line_width(lleg_x, lleg_y, lknee_x, lknee_y, 8);
+         }
+         //draw_sprite_ext(leg_sprite,0,rleg_x,rleg_y,sqrt(sqr(rleg_x-rfoot_x)+sqr(rleg_y-rfoot_y))/32,1,arctan2(rleg_y-rfoot_y,rfoot_x-rleg_x)*180/pi,c_white,1);
+         //draw_sprite_ext(feet,0,rfoot_x,rfoot_y,1,1,facing_direction,c_white,1);
+         if(arm_pose==0){
+         larm_x=(x-18*sin(facing_direction*pi/180))+(x-lfoot_x)*0.3;
+         larm_y=(y-18*cos(facing_direction*pi/180))+(y-lfoot_y)*0.3;
+         rarm_x=(x+18*sin(facing_direction*pi/180))+(x-rfoot_x)*0.3;
+         rarm_y=(y+18*cos(facing_direction*pi/180))+(y-rfoot_y)*0.3;
+    
+         lelbow_x=(lleg_x+larm_x)/2-6*cos(facing_direction*pi/180);
+         lelbow_y=(lleg_y+larm_y)/2+6*sin(facing_direction*pi/180);
+         relbow_x=(rleg_x+rarm_x)/2-6*cos(facing_direction*pi/180);
+         relbow_y=(rleg_y+rarm_y)/2+6*sin(facing_direction*pi/180);
+         }
+         if(arm_pose==1){
+         //1-handed ranged weapon
+         larm_x=x+12*cos((facing_direction+60)*pi/180);
+         larm_y=y-12*sin((facing_direction+60)*pi/180);
+         rarm_x=x+18*cos((facing_direction-20)*pi/180);
+         rarm_y=y-18*sin((facing_direction-20)*pi/180);
+         }
+         if(arm_pose==1){
+         //1-handed ranged weapon
+         larm_x=x+12*cos((facing_direction+60)*pi/180);
+         larm_y=y-12*sin((facing_direction+60)*pi/180);
+         rarm_x=x+18*cos((facing_direction-20)*pi/180);
+         rarm_y=y-18*sin((facing_direction-20)*pi/180); 
+         }
+         if(arm_pose==2){
+         //psychic arm pose
+         larm_x=x+18*cos((facing_direction+20)*pi/180);
+         larm_y=y-18*sin((facing_direction+20)*pi/180);
+         rarm_x=x+12*cos((facing_direction-60)*pi/180);
+         rarm_y=y-12*sin((facing_direction-60)*pi/180); 
+         }
+         if(arm_pose==3){
+         //2-handed ranged weapon
+         larm_x=x+18*cos((facing_direction)*pi/180);
+         larm_y=y-18*sin((facing_direction)*pi/180);
+         rarm_x=x+10*cos((facing_direction)*pi/180);
+         rarm_y=y-10*sin((facing_direction)*pi/180);
+    
+         lelbow_x=x+10*cos((facing_direction+50)*pi/180);
+         lelbow_y=y-10*sin((facing_direction+50)*pi/180);
+         relbow_x=x+10*cos((facing_direction-80)*pi/180);
+         relbow_y=y-10*sin((facing_direction-80)*pi/180);
+         }
+         else{
+         lelbow_x=(lleg_x+larm_x)/2-6*cos(facing_direction*pi/180);
+         lelbow_y=(lleg_y+larm_y)/2+6*sin(facing_direction*pi/180);
+         relbow_x=(rleg_x+rarm_x)/2-6*cos(facing_direction*pi/180);
+         relbow_y=(rleg_y+rarm_y)/2+6*sin(facing_direction*pi/180);
+         }
+
+
+         draw_sprite_ext(lhand_sprite,0,larm_x,larm_y,1,1,facing_direction,c_white,1);
+         draw_sprite_ext(rhand_sprite,0,rarm_x,rarm_y,1,1,facing_direction,c_white,1);
+         //elbow code
+
+
+
+         draw_set_color(larm_color1);
+         draw_line_width(larm_x-2*cos(facing_direction*pi/180), larm_y+2*sin(facing_direction*pi/180), lelbow_x, lelbow_y, 8);
+         draw_set_color(larm_color2);
+         draw_circle(lelbow_x, lelbow_y, 4, false);
+         draw_line_width(lleg_x, lleg_y, lelbow_x, lelbow_y, 8);
+
+
+         draw_set_color(rarm_color1);
+         draw_line_width(rarm_x-2*cos(facing_direction*pi/180), rarm_y+2*sin(facing_direction*pi/180), relbow_x, relbow_y, 8);
+         draw_set_color(rarm_color2);
+         draw_circle(relbow_x, relbow_y, 4, false);
+         draw_line_width(rleg_x, rleg_y, relbow_x, relbow_y, 8);
+         //if(arm_pose>0){
+         if weapon_sprite != noone {
+         draw_sprite_ext(weapon_sprite,0,rarm_x,rarm_y,1,1,facing_direction,c_white,1);
+         }
+         //draw_sprite_ext(arm_sprite,1,x,y,1,1,arm_direction,c_white,1);
+         draw_sprite_ext(head_sprite,0,x,y,1,1,facing_direction,c_white,1);
+         */
     }
 }
--- src/units/Limb.java
+++ src/units/Limb.java
@@ -2,18 +2,19 @@ package units;
 
 import core.Color4d;
 import core.Vec2;
+import graphics.Graphics;
 import graphics.SpriteContainer;
 import graphics.Texture;
 import java.io.IOException;
 
 public class Limb {
-    
+
     public Vec2 pos;
     public Vec2 target;
     public Color4d color1;
     public Color4d color2;
     public Texture sprite;
-    
+
     public Limb(String sprite) {
         pos = new Vec2();
         target = new Vec2();
@@ -23,5 +24,9 @@ public class Limb {
             this.sprite = SpriteContainer.loadSprite(sprite, 1, 1).get(0);
         } catch (IOException ex) {
         }
+    }
+
+    public void draw(double angle) {
+        Graphics.drawSprite(sprite, pos, new Vec2(1, 1), angle, Color4d.WHITE);
     }
 }
