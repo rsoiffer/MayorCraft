176d6de05f8fe9fe6f07707c6c74d5635447bf10
Chris Goodman <chrisgoodman14@gmail.com>
2015-04-10 14:39:23 -0700

    add HousingComponent

---
+++ src/buildings/HousingComponent.java
@@ -0,0 +1,18 @@
+package buildings;
+
+import core.AbstractComponent;
+import movement.PositionComponent;
+
+
+/**
+ *
+ *  @author EendiVidi
+ */
+public class HousingComponent extends AbstractComponent{
+int capacity;
+public HousingComponent(){
+
+}
+
+
+} 
